2d67bd072272db90d9721208243c4beef24a3dae
whuthj <whuthj@163.com>
2016-05-09 09:52:02 +0800

    +添加服务通讯Binder AIDL测试代码
    Signed-off-by: whuthj <whuthj@163.com>

--- app/src/main/java/com/lib/multiproprefs_demo/act/MainActivity.java
+++ app/src/main/java/com/lib/multiproprefs_demo/act/MainActivity.java
@@ -1,13 +1,19 @@
 package com.lib.multiproprefs_demo.act;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.lib.multiproprefs.MPSharedPrefs;
 import com.lib.multiproprefs_demo.R;
+import com.lib.multiproprefs_demo.aidl.IMyService;
 import com.lib.multiproprefs_demo.services.DaemonService;
 
 import java.lang.ref.WeakReference;
@@ -43,6 +49,24 @@ public class MainActivity extends AppCompatActivity {
 
     private Timer mTimer = new Timer();
 
+    private IMyService mIMyService = null;
+    private final ServiceConnection mSerConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mIMyService = IMyService.Stub.asInterface(service);
+            try {
+                mIMyService.getPerson();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mIMyService = null;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +82,18 @@ public class MainActivity extends AppCompatActivity {
         }, 1000, 1000);
 
         DaemonService.startService(getApplicationContext());
+
+        Intent it = new Intent("com.lib.multiproprefs_demo.services.DaemonService");
+        it.setPackage("com.lib.multiproprefs");
+        bindService(it, mSerConn, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (null != mIMyService) {
+
+        }
+        super.onDestroy();
     }
 
     private void refreshText() {
@@ -65,5 +101,14 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = (TextView)findViewById(R.id.txt);
         tv.setText("多进程SharedPrefs--" + sharedPrefs.getString("value", ""));
+
+        if (null == mIMyService) {
+            return;
+        }
+        try {
+            mIMyService.getPerson();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/lib/multiproprefs_demo/services/DaemonService.java
+++ app/src/main/java/com/lib/multiproprefs_demo/services/DaemonService.java
@@ -4,10 +4,19 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
 
 import com.lib.multiproprefs.MPSharedPrefs;
+import com.lib.multiproprefs_demo.aidl.IMyService;
+import com.lib.multiproprefs_demo.aidl.vo.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DaemonService extends Service {
+    private static final String TAG = DaemonService.class.getSimpleName();
 
     public static void startService(Context ctx) {
         if (null == ctx) {
@@ -21,6 +30,40 @@ public class DaemonService extends Service {
             ex.printStackTrace();
         }
     }
+
+    private List<Person> mPersons = new ArrayList<Person>();
+    //这里实现了aidl中的抽象函数
+    private final IMyService.Stub mBinder = new IMyService.Stub() {
+
+        @Override
+        public List<Person> getPerson() throws RemoteException {
+            synchronized (mPersons) {
+                return mPersons;
+            }
+        }
+
+        @Override
+        public void addPerson(Person person) throws RemoteException {
+            synchronized (mPersons) {
+                if (!mPersons.contains(person)) {
+                    mPersons.add(person);
+                }
+            }
+        }
+
+        public boolean onTransact(int code, Parcel data, Parcel reply, int flags)
+                throws RemoteException {
+            String packageName = null;
+            String[] packages = DaemonService.this.getPackageManager().getPackagesForUid(getCallingUid());
+            if (packages != null && packages.length > 0) {
+                packageName = packages[0];
+            }
+            Log.d(TAG, "onTransact: " + packageName);
+
+            return super.onTransact(code, data, reply, flags);
+        }
+
+    };
 
     public DaemonService() {
     }
@@ -52,7 +95,6 @@ public class DaemonService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
