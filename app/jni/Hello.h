4e257b84c3429d02bb61a1613d257c6dc0be00f3
胡俊 <whuthj@163.com>
2016-05-25 22:32:27 +0800

    *修改头文件
    Signed-off-by: 胡俊 <whuthj@163.com>

--- app/jni/Hello.h
+++ app/jni/Hello.h
@@ -1,13 +1,5 @@
 #include <jni.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <utime.h>
-#include <sys/system_properties.h>
-
 #ifndef _HELLO_
 #define _HELLO_
 #ifdef __cplusplus
@@ -15,21 +7,6 @@
 extern "C" {
 #endif
 
-#include <sys/system_properties.h>
-int ACDD_property_get(const char *key, char *value, const char *default_value)
-{
-    int len;
-    len = __system_property_get(key, value);
-    if(len > 0) {
-        return len;
-    }
-
-    if(default_value) {
-        len = strlen(default_value);
-        memcpy(value, default_value, len + 1);
-    }
-    return len;
-}
 JNIEXPORT jstring JNICALL Java_com_lib_multiproprefs_1demo_act_MainActivity_fromJNI(JNIEnv* env, jobject obj);
 
 #ifdef __cplusplus
