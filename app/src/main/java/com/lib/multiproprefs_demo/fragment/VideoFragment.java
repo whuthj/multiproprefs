3f219d8f4778922e113f348ad5119285a806bfcf
胡俊 <whuthj@163.com>
2016-06-01 12:05:06 +0800

    +添加视频测试例子
    Signed-off-by: 胡俊 <whuthj@163.com>

--- app/src/main/java/com/lib/multiproprefs_demo/fragment/VideoFragment.java
+++ app/src/main/java/com/lib/multiproprefs_demo/fragment/VideoFragment.java
@@ -1,14 +1,19 @@
 package com.lib.multiproprefs_demo.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.lib.multiproprefs_demo.R;
 
+import org.libsdl.app.SDLActivity;
+
 public class VideoFragment extends BaseFragment {
 
+    private Button mBtnView;
 
     public VideoFragment() {
         // Required empty public constructor
@@ -20,7 +25,24 @@ public class VideoFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mMainView = inflater.inflate(R.layout.activity_video, container, false);
+        findViewById();
+        init();
+
         return mMainView;
+    }
+
+    private void findViewById() {
+        mBtnView = (Button) mMainView.findViewById(R.id.btnView);
+    }
+
+    private void init() {
+        mBtnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getActivity(), SDLActivity.class);
+                startActivity(it);
+            }
+        });
     }
 
 }
