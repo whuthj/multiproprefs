b9ad85642ac8bcd71fa7636b32b8545d6dd6f3b0
胡俊 <whuthj@163.com>
2016-06-10 14:42:42 +0800

    *修复ffmpege编译配置
    Signed-off-by: 胡俊 <whuthj@163.com>

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -1,2 +1,6 @@
+#adding APP_MODULES lines tells system only compile changed files.
+APP_MODULES :=  avcodec avdevice avfilter avformat avresample avutil swresample swscale
+APP_MODULES += SDL2 main test
+APP_ABI := armeabi-v7a 
+APP_PLATFORM := android-9
 APP_STL := gnustl_static
-APP_PLATFORM := android-9  
